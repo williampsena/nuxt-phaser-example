dbfd9f25a7c9926f45d779b5a1fc481649012178
William Sena <me@willsena.dev>
2023-05-09 13:27:52 -0300

    Upgrade version

--- typings/plugin.d.ts
+++ typings/plugin.d.ts
@@ -1,2 +1,3 @@
-/// <reference path="../node_modules/nuxtjs-phaser/types/index.d.ts"/>
-/// <reference path="../node_modules/nuxtjs-phaser/types/vue-shim.d.ts"/>
+/// <reference path="../node_modules/nuxtjs-phaser/@types/index.d.ts"/>
+/// <reference path="../node_modules/nuxtjs-phaser/@types/global.d.ts"/>
+/// <reference path="../node_modules/nuxtjs-phaser/@types/vue-shim.d.ts"/>
